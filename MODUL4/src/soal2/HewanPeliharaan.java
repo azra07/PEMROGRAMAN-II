eb349c7351b562ce87908524b92198284bc311c0
azra07 <136769399+azra07@users.noreply.github.com>
2025-10-29 19:00:50 +0700

    Update HewanPeliharaan.java

--- MODUL4/src/soal2/HewanPeliharaan.java
+++ MODUL4/src/soal2/HewanPeliharaan.java
@@ -3,10 +3,12 @@ package soal2;
 public class HewanPeliharaan {
 	private String nama;
     private String ras;
+
     public HewanPeliharaan(String r, String n) {
         this.nama = n;
         this.ras = r;
     }
+
     void display() {
         System.out.println("\nDetail Hewan Peliharaan:");
         System.out.println("Nama hewan peliharaanku adalah: " + this.nama);
@@ -14,3 +16,4 @@ public class HewanPeliharaan {
     }
 
 }
+
