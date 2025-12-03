dbff93cab932572e53f6de06f7e2ed6ad12ed854
azra07 <136769399+azra07@users.noreply.github.com>
2025-12-03 20:02:23 +0700

    Delete PRAKTIKUM7/src directory

--- PRAKTIKUM7/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Praktikum7 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    
-    opens application to javafx.graphics, javafx.fxml;
-    opens Controller to javafx.fxml;
-    opens Model to javafx.base;
-}
